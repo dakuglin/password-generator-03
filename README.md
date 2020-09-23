0bf2e8a89cd24f1f5743cfafce4840075646cadc
Dana Kuglin <dakuglin@gmail.com>
2020-09-23 15:32:43 -0600

    added .html, .js, .css files

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # password-generator-03
+
+* The purpose of this homework is to create a random passowrd generator. The user of the site will be able to select specific parameters needed for the password such as:
+
+* Length of desired password (ranging from 8 characters to 128 characters)
+* Needing or not needing lowercase letters
+* Needing or not needing uppercase letters
+* Needing or not needing numbers 
+* Needing or not needing special symbols 
+
+* Pseudocode:
+1) prompt user to define the length of password they need 
+2) User selects what other password parameters they need
+User clicks the button to generate a password
+User is presented with a series of prompts for password criteria
+
+User selects which criteria to include in the password:
+    Length of the password
+    THEN user chooses a length of at least 8 characters and no more than 128 characters
+    Character types needed in password
+    THEN user chooses lowercase, uppercase, numeric, and/or special characters
+
+
+
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
---
+++ script.js
@@ -0,0 +1,57 @@
+// Assignment Code
+
+// Global variables 
+//-----------------------------------------------------------------------------------------------
+
+// var userPasswordLength = document.querySelector(); // HOW DOES THIS LINK TO WHAT THEY CHOOSE FOR A NUMBER???
+// console.log(userPasswordLength);
+
+var lowercase = 'abcdefghijklmnopqrstuvwxyz';
+console.log(lowercase.length);
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var number = '123456789';
+var symbol = '!"#%&()*+-./:<?^[]{}~';
+
+
+//var generateBtn = document.querySelector(".orderedList"); // querySelector() returns the first element that matches CSS selectors
+//console.log(generateBtn);
+
+
+// var userDefinedPassLength = prompt("Specify the desired length of new password (min 8 characters , max 128 characters)") //prompts user to input the desired length of password
+// console.log(userDefinedPassLength);
+
+ 
+// Functions 
+// var userDefined
+// -----------------------------------------------------------------------------------------------
+
+// Write password to the #password input
+function writePassword() {  //function is a set of statements that performs tasks or calculates values, needs to take some inputs (password requirements) and retun some output (new valid password)
+
+  
+//---------------------------------------- code logic -------------------------------------------
+  // var passwordText = ["A","B", "C", "D", "E", "a", "b", "c", "d", "e", "1", "2", "3", "4", "5", "6", "7", "8", "9", "!", "&", "(", ")", "@", "#", "$", ":", "^",];
+
+  // for (i=0; i < 10; i++) {
+  //   var passwordText = Math.floor(Math.random() * passwordText.length + 8);
+    
+  // }
+
+  // var passwordLength = 
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+
+
+
+
+// generateBtn.addEventListener("click", writePassword); //event listener activates when user clicks button
+
+// function writePassword () {
+  
+// }
